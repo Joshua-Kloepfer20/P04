87917b280c2f33edf2a588587c5e964e23e730e7
Lia-Nelson <jnelson20@stuy.edu>
2022-06-02 20:55:52 -0400

    code associated with arrow keys cleaned up, comments added, outline of code required for move added (updating server data and consuming other users), DD updated with new game mechanics decided upon (not in repo yet because group has access to live DD and process of converting to pdf time consuming)

--- test/test3.js
+++ test/test3.js
@@ -1,56 +1,72 @@
 // Getting key presses
-document.onkeydown = function(){arrowChange(true)};
-document.onkeyup = function(){arrowChange(false)};
+// Calls function that sees if an arrow key was pressed along with parameter
+// stating whether a keydown (true) or keyup event occured
+document.onkeydown = function(){arrowChange(true, "User")};
+document.onkeyup = function(){arrowChange(false, "User")};
 
-  function arrowChange(isDown) {
-  // Create scheduler for each and have up key cancel
+  function arrowChange(isDown, user) {
   var key = event.keyCode;
+      // determines if an arrow key was pressed
       switch (key) {
          case 37: case 38: case 39: case 40:
+            // if a keydown event occured, starts movement by calling change
+            // timer with isStart as true
             if (isDown) {
-              startMove("Test", key);
+              // user is only specified in instances of changeTimer so that it
+              // is there when move is called and can be used to update server
+              // data
+              changeTimer(user, key, true);
             }
+            // if the event was keyup, stops movement by calling change timer
+            // with isStart as false
             else {
-              stopMove("Test", key);
+              changeTimer(user, key, false);
             }
             break;
       }
    };
 
+   // variables to allow functions to be called at appropriate interval
+   // separately for left, right, up and down movement
    var timerL;
    var timerU;
    var timerR;
    var timerD;
 
-   function startMove(user, key) {
-   	console.log("starting?" + user + key);
-    changeTimer(user, key, true);
-   }
-
+   // logs that a move occured, upon integration will update server data
    function move(user, key) {
      console.log("move" + user + key);
-   }
-
-   // stops metronome by clearing interval
-   function stopMove(user, key) {
-     console.log("stopping?");
-     // console.log(timer);
-     changeTimer(user, key, false);
+     // at new position test if can eat any (other users will do same)
+     // go through data to see if the inner portion (contained by the circle
+     // with in same center but Math.sqrt(9/10) * radius or 90% of the mass) of
+     // this user touches the inner portion of the other
+     // if so, checks to see if the mass of the user is <= 90% of this one
+     // if it is, this user consumes other and recieves 90% of mass and radius
+     // recalculated
    }
 
    function changeTimer(user, key, isStart) {
      switch (key) {
+       // for left key
         case 37:
+           // if starting movement
            if (isStart) {
              console.log("starting" + user + key);
+             // clears interval (stops calling function at interval specified in
+             // ms) to avoid multiple assignments to same variable
              timerL = clearInterval(timerL);
+             // sets appropriate interval for the function move to be called at
+             // with user and key as parameters
              timerL = setInterval(move, 1, user, key);
            }
+           // if stopping movement
            else {
              console.log("stopping" + user + key);
+             // clears interval so move is no longer called
              timerL = clearInterval(timerL);
            }
            break;
+        // same as left key but for up
         case 38:
            if (isStart) {
              timerU = clearInterval(timerU);
@@ -62,6 +78,7 @@ document.onkeyup = function(){arrowChange(false)};
              timerU = clearInterval(timerU);
            }
            break;
+        // same as other arrow keys but for right
         case 39:
         if (isStart) {
           timerR = clearInterval(timerR);
@@ -73,6 +90,7 @@ document.onkeyup = function(){arrowChange(false)};
           timerR = clearInterval(timerR);
         }
            break;
+        // same as other arrow keys but for down
         case 40:
         if (isStart) {
           timerD = clearInterval(timerD);
