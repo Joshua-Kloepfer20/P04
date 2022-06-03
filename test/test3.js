f1687d1f50700df5e941ab0b1c176b3194e009f7
Lia-Nelson <jnelson20@stuy.edu>
2022-06-03 08:13:16 -0400

    user parameter removed from arrow key movement (server will know which client is sending)

--- test/test3.js
+++ test/test3.js
@@ -1,10 +1,10 @@
 // Getting key presses
 // Calls function that sees if an arrow key was pressed along with parameter
 // stating whether a keydown (true) or keyup event occured
-document.onkeydown = function(){arrowChange(true, "User")};
-document.onkeyup = function(){arrowChange(false, "User")};
+document.onkeydown = function(){arrowChange(true)};
+document.onkeyup = function(){arrowChange(false)};
 
-  function arrowChange(isDown, user) {
+  function arrowChange(isDown) {
   var key = event.keyCode;
       // determines if an arrow key was pressed
       switch (key) {
@@ -12,15 +12,12 @@ document.onkeyup = function(){arrowChange(false, "User")};
             // if a keydown event occured, starts movement by calling change
             // timer with isStart as true
             if (isDown) {
-              // user is only specified in instances of changeTimer so that it
-              // is there when move is called and can be used to update server
-              // data
-              changeTimer(user, key, true);
+              changeTimer(key, true);
             }
             // if the event was keyup, stops movement by calling change timer
             // with isStart as false
             else {
-              changeTimer(user, key, false);
+              changeTimer(key, false);
             }
             break;
       }
@@ -34,8 +31,8 @@ document.onkeyup = function(){arrowChange(false, "User")};
    var timerD;
 
    // logs that a move occured, upon integration will update server data
-   function move(user, key) {
-     console.log("move" + user + key);
+   function move(key) {
+     console.log("move" +  key);
      // at new position test if can eat any (other users will do same)
      // go through data to see if the inner portion (contained by the circle
      // with in same center but Math.sqrt(9/10) * radius or 90% of the mass) of
@@ -45,23 +42,23 @@ document.onkeyup = function(){arrowChange(false, "User")};
      // recalculated
    }
 
-   function changeTimer(user, key, isStart) {
+   function changeTimer(key, isStart) {
      switch (key) {
        // for left key
         case 37:
            // if starting movement
            if (isStart) {
-             console.log("starting" + user + key);
+             console.log("starting" + key);
              // clears interval (stops calling function at interval specified in
              // ms) to avoid multiple assignments to same variable
              timerL = clearInterval(timerL);
              // sets appropriate interval for the function move to be called at
-             // with user and key as parameters
-             timerL = setInterval(move, 1, user, key);
+             // with key as parameter
+             timerL = setInterval(move, 1, key);
            }
            // if stopping movement
            else {
-             console.log("stopping" + user + key);
+             console.log("stopping" + key);
              // clears interval so move is no longer called
              timerL = clearInterval(timerL);
            }
@@ -70,11 +67,11 @@ document.onkeyup = function(){arrowChange(false, "User")};
         case 38:
            if (isStart) {
              timerU = clearInterval(timerU);
-             console.log("starting" + user + key);
-             timerU = setInterval(move, 1, user, key);
+             console.log("starting"  + key);
+             timerU = setInterval(move, 1, key);
            }
            else {
-             console.log("stopping" + user + key);
+             console.log("stopping" + key);
              timerU = clearInterval(timerU);
            }
            break;
@@ -82,11 +79,11 @@ document.onkeyup = function(){arrowChange(false, "User")};
         case 39:
         if (isStart) {
           timerR = clearInterval(timerR);
-          console.log("starting" + user + key);
-          timerR = setInterval(move, 1, user, key);
+          console.log("starting" + key);
+          timerR = setInterval(move, 1, key);
         }
         else {
-          console.log("stopping" + user + key);
+          console.log("stopping" + key);
           timerR = clearInterval(timerR);
         }
            break;
@@ -94,11 +91,11 @@ document.onkeyup = function(){arrowChange(false, "User")};
         case 40:
         if (isStart) {
           timerD = clearInterval(timerD);
-          console.log("starting" + user + key);
-          timerD = setInterval(move, 1, user, key);
+          console.log("starting" + key);
+          timerD = setInterval(move, 1, key);
         }
         else {
-          console.log("stopping" + user + key);
+          console.log("stopping" + key);
           timerD = clearInterval(timerD);
         }
            break;
